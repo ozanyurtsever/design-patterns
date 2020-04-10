620a2662475f2cb3ef3041bd5419040fdcd43088
Ozan Yurtsever <ozany@atptech.net>
2020-04-10 16:58:33 +0300

    Composite Pattern

---
+++ Composite/Composite.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public interface IComponent
+    {
+        void Draw();
+        void Add(IComponent component);
+        void Remove(IComponent component);
+    }
+
+    public class Label : IComponent
+    {
+        public void Draw()
+        {
+            Console.WriteLine($"This is a {nameof(Label)}");
+        }
+
+        public void Add(IComponent component)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void Remove(IComponent component)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    public class TextBox : IComponent
+    {
+        public void Draw()
+        {
+            Console.WriteLine($"This is a {nameof(TextBox)}");
+        }
+        public void Add(IComponent component)
+        {
+            throw new NotSupportedException();
+        }
+        public void Remove(IComponent component)
+        {
+            throw new NotSupportedException();
+
+        }
+    }
+
+    public class Panel : IComponent
+    {
+        public List<IComponent> Components { get; set; }
+
+        public Panel()
+        {
+            Components = new List<IComponent>();
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine($"This is a {nameof(Panel)}");
+
+            Console.WriteLine("Child Components:");
+            Console.WriteLine("-------------------------------------");
+            foreach ( var item in Components)
+            {
+                item.Draw();
+            }
+        }
+        public void Add(IComponent component)
+        {
+            Components.Add(component);
+        }
+        public void Remove(IComponent component)
+        {
+            Components.Remove(component);
+
+        }
+    }
+}
---
+++ Composite/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Panel panel = new Panel();
+            panel.Add(new TextBox());
+            panel.Add(new Label());
+
+            panel.Draw();
+
+            Console.ReadLine();
+        }
+    }
+}
